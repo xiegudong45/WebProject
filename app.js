5958c30d95e8e71ddf2b5df424827ae0f7ee21fc
xiegudong45 <xshxiegudong45@gmail.com>
2019-05-05 22:02:40 -0700

    changed port

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.use('/', indexRoutes);
 app.use('/campgrounds',campgroundRoutes);
 app.use('/campgrounds/:id/comments',commentRoutes);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("The YelpCamp App has been launched!");
 });
 
